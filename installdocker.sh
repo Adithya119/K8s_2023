e5325f34bdf3cedc370a9c16f7b35e398713a732
Adi <kariveda11@gmail.com>
2023-04-05 20:21:30 +0530

    installdocker-new.sh

--- installdocker.sh
+++ installdocker.sh
@@ -3,4 +3,3 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 sudo usermod -aG docker ubuntu
 
-# installed from here --> https://docs.docker.com/engine/install/debian/#install-using-the-convenience-script
