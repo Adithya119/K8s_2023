a67bd77001766db217e431ad061469b8fb613b59
Adi <kariveda11@gmail.com>
2023-04-06 09:15:25 +0530

    comments

--- installdocker.sh
+++ installdocker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+sudo apt update     # always the 1st command
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 sudo usermod -aG docker ubuntu
 
+# this script is working as of 6th Apr 2023
