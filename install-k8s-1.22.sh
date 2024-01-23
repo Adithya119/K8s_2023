4d7c8d2c63efd6ff2b3335655be3c7da4e5c3269
Adi <kariveda11@gmail.com>
2024-01-23 18:14:09 +0530

    comments

--- install-k8s-1.22.sh
+++ install-k8s-1.22.sh
@@ -7,6 +7,9 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/
 sudo apt-get update -q && \
 sudo apt-get install -qy kubelet=1.22.3-00 kubectl=1.22.3-00 kubeadm=1.22.3-00
 sudo apt-mark hold kubelet kubeadm kubectl
+# Later do kubeadm init --pod-network-cidr=192.168.0.0/16
+
+
 # swapoff -a
 
 # installing k8s version 1.22.3-00 because after 1.23, docker is not compatible for k8s as container runtime
